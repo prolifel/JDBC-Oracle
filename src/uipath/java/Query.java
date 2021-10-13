9863ad8bcc19564e91b461197d3bb1fce9b037a1
Clement Prolifel Priyatama <clementprolifel@gmail.com>
2021-10-13 15:57:43 +0700

    add query result to xmlstring. still static query

--- src/uipath/java/Query.java
+++ src/uipath/java/Query.java
@@ -1,7 +1,31 @@
 package uipath.java;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.ResultSetMetaData;
+
 import java.util.Properties;
+
+import javax.sql.RowSetMetaData;
+import javax.sql.rowset.WebRowSet;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.sun.rowset.WebRowSetImpl;
+
 import java.io.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -82,7 +106,7 @@ public class Query {
         return connection;
     }
 
-    public static JSONObject uipathSelectQuery(Connection connection) {
+    public static JSONObject uipathSelectQueryJSON(Connection connection) {
         String query = "select * from regions";
         JSONObject json = new JSONObject();
 
@@ -108,6 +132,90 @@ public class Query {
         return json;
     }
 
+    public static String uipathSelectQueryWebRowSet(Connection connection) throws SQLException {
+        String query = "select * from regions";
+        WebRowSet webRS = new WebRowSetImpl();
+        StringWriter sw = new StringWriter();
+        webRS.setCommand(query);
+        webRS.execute(connection);
+        webRS.writeXml(sw);
+        System.out.println(sw.toString());
+        webRS.close();
+        
+        // try {
+        //     // RowSetMetaData rsMD = (RowSetMetaData) webRS.getMetaData();
+        //     // System.out.println("rsMD=" + rsMD);
+        //     // if (rsMD == null) {
+        //     //     System.out.println("vendor does not support RowSetMetaData");
+        //     //   } else {
+        //     //     int columnCount = rsMD.getColumnCount();
+        //     //     System.out.println("columnCount=" + columnCount);
+        //     //   };
+        // } catch (Exception e) {
+        //     //TODO: handle exception   
+        //     e.printStackTrace();
+        // }
+        return sw.toString();
+    }
+
+    public static String uipathSelectQueryDocument(Connection connection) throws ParserConfigurationException, SQLException {
+        String query = "select * from regions";
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.newDocument();
+        StringWriter writer = new StringWriter();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            
+            Element results = doc.createElement("Results");
+            doc.appendChild(results);
+            
+            ResultSetMetaData rsmd = resultSet.getMetaData();
+            int colCount = rsmd.getColumnCount();
+    
+            while (resultSet.next())
+            {
+                Element row = doc.createElement("Row");
+                results.appendChild(row);
+    
+                for (int i = 1; i <= colCount; i++)
+                {
+                    String columnName = rsmd.getColumnName(i);
+                    Object value = resultSet.getObject(i);
+    
+                    Element node = doc.createElement(columnName);
+                    node.appendChild(doc.createTextNode(value.toString()));
+                    row.appendChild(node);
+                }
+            }
+            
+            DOMSource domSource = new DOMSource(doc);
+            StreamResult result = new StreamResult(writer);
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.transform(domSource, result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return writer.toString();
+    }
+
+    public static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+    
+        transformer.transform(new DOMSource(doc), 
+             new StreamResult(new OutputStreamWriter(out, "UTF-8")));
+    }
+
     public static void main(String[] args) throws SQLException {
         Query query;
         Connection connection = null;
@@ -128,7 +236,10 @@ public class Query {
             // create sql command here
             // Query.selectQuery(connection);
 
-            System.out.println(Query.uipathSelectQuery(connection));
+            // System.out.println(Query.uipathSelectQueryJSON(connection));
+            // Query.uipathSelectQueryWebRowSet(connection);
+            System.out.println(Query.uipathSelectQueryDocument(connection));
+            // printDocument(Query.uipathSelectQueryDocument(connection), System.out);
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (Exception e) {
